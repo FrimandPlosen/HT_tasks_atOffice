9fde1931ad465dfd683d7592d731a4bf3d1e62ef
Frimand Pløsen <frimand.plosen@enoro.com>
2020-11-03 10:13:59 +0100

    first commit to repo. contains an empty solution so far

---
+++ HT_tasks_atoffice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT_tasks_atoffice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
