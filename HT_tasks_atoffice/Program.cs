cfe6fec35be9b1840256f5c0e2c8d4f00e031c7e
Frimand Pløsen <frimand.plosen@enoro.com>
2020-11-03 15:02:44 +0100

    objects can be mapped from namespace B to namespace A

--- HT_tasks_atoffice/Program.cs
+++ HT_tasks_atoffice/Program.cs
@@ -8,16 +8,6 @@ namespace HT_tasks_atoffice
 {
     class Program
     {
-        public static NamespaceA.Person MapManuallyBA(NamespaceB.Person src)
-        {
-            NamespaceA.Person dest = new NamespaceA.Person();
-
-            if (src == null)
-                return null;
-
-
-            return null;
-        }
 
         public static NamespaceB.Person MapManuallyAB(NamespaceA.Person src)
         {
@@ -75,10 +65,100 @@ namespace HT_tasks_atoffice
                 return dest;
             }
         }
+        public static NamespaceA.Person MapManuallyBA(NamespaceB.Person src)
+        {
+            NamespaceA.Person dest = new Person();
+
+            if (src == null)
+            {
+                return null;
+            }
+
+            string[] split = src.Name.Split(' ');
+            dest.FirstName = split[0];
+            dest.LastName = split[1];
+
+            int yearOfBirth = DateTime.Now.Year - src.Age;
+            dest.YearOfBirth = yearOfBirth.ToString();
+            
+            bool isParseable = Enum.TryParse(src.Sex, out Sex sex);
+            if (isParseable == true)
+            {
+                dest.Sex = sex;
+            }
+
+            if (src.Relationships != null)
+            {
+                dest.Relationships = new List<Relationship>();
+                for (int i = 0; i < src.Relationships.Length; i++)
+                {
+                    dest.Relationships.Add(MapManuallyBA(src.Relationships[i]));
+                }
+            } 
+            return dest;
+         }
+        public static NamespaceA.Relationship MapManuallyBA(NamespaceB.Relationship src)
+        {
+            if (src == null)
+            {
+                return null;
+            }
+
+            NamespaceA.Relationship dest = new Relationship();
+            dest.RelationshipWith = MapManuallyBA(src.RelationshipWith);
+            
+            dest.RelationShipType = new List<RelationShipType>();
+            for (int i = 0; i < src.RelationShipType.Length; i++)
+            {
+                bool isParseable = Enum.TryParse(src.RelationShipType[i], out RelationShipType relationshiptype);
+                if (isParseable == true)
+                {
+                    dest.RelationShipType.Add(relationshiptype);
+                }
+            }
+            return dest;
+        }
 
 
         static void Main(string[] args)
         {
+            NamespaceB.Person test1 = new NamespaceB.Person
+            {
+                Age = 30,
+                Name = "Joachim Hansen",
+                Sex = "Male",
+                Relationships = new NamespaceB.Relationship[]
+                 {
+                     new NamespaceB.Relationship
+                     {
+                          RelationshipWith = new NamespaceB.Person
+                          {
+                               Age = 27,
+                               Name = "Jonas Berg",
+                               Sex = "Male",
+                               Relationships = null,
+
+
+                          },
+                          RelationShipType = new string[]
+                          {
+                                "Friend"
+                          }
+                     }
+                 }
+            };
+            NamespaceA.Person person1 = MapManuallyBA(test1);
+            Console.WriteLine("First Name: " + person1.FirstName);
+            Console.WriteLine("Last Name: " + person1.LastName);
+            Console.WriteLine("Year of Birth: " + person1.YearOfBirth);
+            Console.WriteLine("Sex: " + person1.Sex);
+            for (int j = 0; j<person1.Relationships.Count; j++)
+            {
+                Console.WriteLine("Relationships: " + person1.Relationships[j].RelationshipWith.FirstName
+                                              + " " + person1.Relationships[j].RelationshipWith.LastName);
+                Console.WriteLine("Relationship Type: " + person1.Relationships[j].RelationShipType[j]);
+            }
+       
             NamespaceA.Person test2 = new NamespaceA.Person
             {
                 FirstName = "Joachim",
@@ -106,18 +186,16 @@ namespace HT_tasks_atoffice
                  }
             };
             
-            NamespaceB.Person dest = MapManuallyAB(test2);
-            Console.WriteLine("Name: " + dest.Name);
-            Console.WriteLine("Age: " + dest.Age);
-            Console.WriteLine("Sex: " + dest.Sex);
-            for (int i = 0; i < dest.Relationships.Length; i++)
+            NamespaceB.Person person2 = MapManuallyAB(test2);
+            Console.WriteLine("////////////////////////////////////");
+            Console.WriteLine("Name: " + person2.Name);
+            Console.WriteLine("Age: " + person2.Age);
+            Console.WriteLine("Sex: " + person2.Sex);
+            for (int i = 0; i < person2.Relationships.Length; i++)
             {
-                Console.WriteLine("Relationship: " + dest.Relationships[i].RelationshipWith.Name);
-                Console.WriteLine("Relationship Type: " + dest.Relationships[i].RelationShipType[i]);
+                Console.WriteLine("Relationship: " + person2.Relationships[i].RelationshipWith.Name);
+                Console.WriteLine("Relationship Type: " + person2.Relationships[i].RelationShipType[i]);
             }
-
-
-
         }
     }
 }
