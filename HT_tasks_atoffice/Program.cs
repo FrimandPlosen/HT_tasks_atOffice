193d9213147cec62bc47b64b69ab8a6a56faf795
Frimand Pløsen <frimand.plosen@enoro.com>
2020-11-04 12:02:01 +0100

    some comment code

--- HT_tasks_atoffice/Program.cs
+++ HT_tasks_atoffice/Program.cs
@@ -9,22 +9,22 @@ namespace HT_tasks_atoffice
     class Program
     {
 
-        public static NamespaceB.Person MapManuallyAB(NamespaceA.Person src)        //Maps the Person object from NamespaceA -> NamespaceB
+        public static NamespaceB.Person MapManuallyAB(NamespaceA.Person src)                //Maps the Person object from NamespaceA -> NamespaceB
         {
             NamespaceB.Person dest = new NamespaceB.Person();
 
-            if (src == null)                                                        // Makes sure code doesnt run if there isnt a person to map
+            if (src == null)                                                                // Makes sure code doesnt run if there isnt a person to map
             {
                 return null;
             }
             else
             {
-                dest.Name = src.FirstName + " " + src.LastName;                     // Combines FirstName and LastName into one string
+                dest.Name = src.FirstName + " " + src.LastName;                             // Combines FirstName and LastName into one string
 
                 bool isParseable = Int32.TryParse(src.YearOfBirth, out int yearOfBirth);    //checks to see if the string "YearOfBirth" is a number, and stores it in a int
                 if (isParseable == true)
                 {
-                    dest.Age = DateTime.Now.Year - yearOfBirth;                         // if it is, calculates age from current date and yearOfBirth
+                    dest.Age = DateTime.Now.Year - yearOfBirth;                             // if it is, calculates age from current date and yearOfBirth
                 }
                 else
                 {
@@ -40,27 +40,27 @@ namespace HT_tasks_atoffice
                     for (int i = 0; i < src.Relationships.Count(); i++)
                     {
                         dest.Relationships[i] = new NamespaceB.Relationship();                  // Creates a new object in the array to store info in
-                        dest.Relationships[i] = MapManuallyAB(src.Relationships[i]);          
+                        dest.Relationships[i] = MapManuallyAB(src.Relationships[i]);            // Calls the overloaded function to handle elements in the Relationship array
                     }
                 }
                 return dest;
             }
         }
-        public static NamespaceB.Relationship MapManuallyAB(NamespaceA.Relationship src)
-        {
+        public static NamespaceB.Relationship MapManuallyAB(NamespaceA.Relationship src)    // Deals with elements in the Relationships list/array in the mapping process A->B
+        {                                                                                   // from NamespaceA -> NamespaceB
             if (src == null)
             {
                 return null;
             }
             else
             {
-                NamespaceB.Relationship dest = new NamespaceB.Relationship();
-                dest.RelationshipWith = MapManuallyAB(src.RelationshipWith);
+                NamespaceB.Relationship dest = new NamespaceB.Relationship();               // creates a new object to store info in
+                dest.RelationshipWith = MapManuallyAB(src.RelationshipWith);                // sends that back to the first function to map over that persons info.
 
-                if (src.RelationShipType != null)
+                if (src.RelationShipType != null)                                           // Check to make sure the person actually has a relationship type to save some work
                 {
                     dest.RelationShipType = new string[src.RelationShipType.Count()];
-                    for (int i = 0; i < src.RelationShipType.Count(); i++)
+                    for (int i = 0; i < src.RelationShipType.Count(); i++)                  // goes through the RelationshipType and maps it over as a String for NamespaceB
                     {
                         dest.RelationShipType[i] = src.RelationShipType[i].ToString();
                     }
@@ -68,7 +68,7 @@ namespace HT_tasks_atoffice
                 return dest;
             }
         }
-        public static NamespaceA.Person MapManuallyBA(NamespaceB.Person src)
+        public static NamespaceA.Person MapManuallyBA(NamespaceB.Person src)                // Maps the Person object from NamespaceB -> NamespaceA
         {
             NamespaceA.Person dest = new Person();
 
@@ -77,30 +77,30 @@ namespace HT_tasks_atoffice
                 return null;
             }
 
-            string[] split = src.Name.Split(' ');
+            string[] split = src.Name.Split(' ');                               // Splits the Name into a FirstName and LastName, split decided by an empty space
             dest.FirstName = split[0];
             dest.LastName = split[1];
 
-            int yearOfBirth = DateTime.Now.Year - src.Age;
-            dest.YearOfBirth = yearOfBirth.ToString();
+            int yearOfBirth = DateTime.Now.Year - src.Age;                      // Calculates yearOfBirth
+            dest.YearOfBirth = yearOfBirth.ToString();                          // changes it from int to String to correspond with NamespaceA
             
-            bool isParseable = Enum.TryParse(src.Sex, out Sex sex);
+            bool isParseable = Enum.TryParse(src.Sex, out Sex sex);             // Checks to see if Sex from NamespaceB corresponds with an option in the enumeration in NamespaceA
             if (isParseable == true)
             {
-                dest.Sex = sex;
+                dest.Sex = sex;                                                 // if yes then map it over
             }
 
-            if (src.Relationships != null)
+            if (src.Relationships != null)                                      // checks to see if there is actually relationships to do work on
             {
                 dest.Relationships = new List<Relationship>();
                 for (int i = 0; i < src.Relationships.Length; i++)
                 {
-                    dest.Relationships.Add(MapManuallyBA(src.Relationships[i]));
+                    dest.Relationships.Add(MapManuallyBA(src.Relationships[i]));    // Adds the objects to the list after they've passed through the Function that deals with relationships
                 }
             } 
             return dest;
          }
-        public static NamespaceA.Relationship MapManuallyBA(NamespaceB.Relationship src)
+        public static NamespaceA.Relationship MapManuallyBA(NamespaceB.Relationship src)    // deals with elements in the Relationships list/array in the mapping process B->
         {
             if (src == null)
             {
@@ -128,7 +128,7 @@ namespace HT_tasks_atoffice
 
         static void Main(string[] args)
         {
-            NamespaceB.Person test1 = new NamespaceB.Person
+            NamespaceB.Person test1 = new NamespaceB.Person             // Test data
             {
                 Age = 30,
                 Name = "Joachim Hansen",
@@ -152,8 +152,8 @@ namespace HT_tasks_atoffice
                           }
                      }
                  }
-            };
-            NamespaceA.Person person1 = MapManuallyBA(test1);
+            };         
+            NamespaceA.Person person1 = MapManuallyBA(test1);           // Map it over to A then display it
             Console.WriteLine("First Name: " + person1.FirstName);
             Console.WriteLine("Last Name: " + person1.LastName);
             Console.WriteLine("Year of Birth: " + person1.YearOfBirth);
@@ -165,7 +165,7 @@ namespace HT_tasks_atoffice
                 Console.WriteLine("Relationship Type: " + person1.Relationships[j].RelationShipType[j]);
             }
        
-            NamespaceA.Person test2 = new NamespaceA.Person
+            NamespaceA.Person test2 = new NamespaceA.Person         // Test data
             {
                 FirstName = "Joachim",
                 LastName = "Hansen",
@@ -190,9 +190,9 @@ namespace HT_tasks_atoffice
                              }
                       }
                  }
-            };
+            };      
             
-            NamespaceB.Person person2 = MapManuallyAB(test2);
+            NamespaceB.Person person2 = MapManuallyAB(test2);            // Maps test data over to B and displays it
             Console.WriteLine("////////////////////////////////////");
             Console.WriteLine("Name: " + person2.Name);
             Console.WriteLine("Age: " + person2.Age);
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -14,8 +14,9 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<NamespaceA.class1, NamespaceB.class1>());
-            NamespaceA.class1 Aclass1 = new NamespaceA.class1
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<NamespaceA.class1, NamespaceB.class1>()); // Sets up the automapper to go from NamespaceA -> NamespaceB
+            
+            NamespaceA.class1 Aclass1 = new NamespaceA.class1           // Test data
             {
                 Foo = "I am from Namespace A",
                 Bar = 1,
@@ -32,14 +33,14 @@ namespace Task4
                 
             };
 
-            Console.WriteLine("Data from NamespaceA: Foo: " + Aclass1.Foo + ", Bar: " + Aclass1.Bar + ", Qux: " + Aclass1.Baz.Qux
+            Console.WriteLine("Data from NamespaceA: Foo: " + Aclass1.Foo + ", Bar: " + Aclass1.Bar + ", Qux: " + Aclass1.Baz.Qux       //Displays Test Data from the NamespaceA object to make sure it initalised properly
                               + ", Garply: " + Aclass1.Baz.Quux.Garply + ", Waldo: " + Aclass1.Baz.Quux.Waldo);
+            
             NamespaceB.class1 Bclass1 = new NamespaceB.class1 { };
+            var mapper = config.CreateMapper();                             // Creates the mapper instnace
+            Bclass1 = mapper.Map<NamespaceB.class1>(Aclass1);               // Maps over to a object in NamespaceB
 
-            var mapper = config.CreateMapper();
-            Bclass1 = mapper.Map<NamespaceB.class1>(Aclass1);
-
-            Console.WriteLine("Data from NamespaceB: Foo: " + Bclass1.Foo + ", Bar: " + Bclass1.Bar + ", Qux: " + Bclass1.Baz.Qux
+            Console.WriteLine("Data from NamespaceB: Foo: " + Bclass1.Foo + ", Bar: " + Bclass1.Bar + ", Qux: " + Bclass1.Baz.Qux       // displays mapped over data for easy comparison to data in NamespaceA object
                               + ", Garply: " + Bclass1.Baz.Quux.Garply + ", Waldo: " + Bclass1.Baz.Quux.Waldo);
         }
     }
